f854f94758656adfc6a55472d41ea027097590a1
Nathan Murzello <nathanthekyle@gmail.com>
2020-11-10 23:50:21 -0500

    replaced

--- newdepartmentphp.php
+++ newdepartmentphp.php
@@ -7,28 +7,29 @@
   </head>
   
   <body>
+    <!--Script to establish a connection with the database and insert user info.-->
     <?php
-    $servername = "localhost";
-    $username = "technonauts";
-    $password = "bonnie123&";
-    $dbname = "technonauts";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        echo "Operation unsuccessful: Department was not added, please try again.<br>";
-        die("Connection failed: " . $conn->connect_error);
-    }
+
+    require 'vendor/autoload.php';
+    $client = new MongoDB\Client(
+      'mongodb+srv://techno:techno123@cluster0.k9zfj.mongodb.net/EmployeeSystem?retryWrites=true&w=majority');
+    $collection=$client->selectCollection('EmployeeSystem','Department');
     
+    
+    //the approved variable holds how many fields have had their input types approved 
     $approved = 0;
     
+    //create variables to hold values from the input form
     $name = $_POST['name'];
     $id = $_POST['id'];
+    $manager = $_POST['manager'];
     $location = $_POST['location'];
-    $form_result = $_POST['form'];
     
+    //regex pattern to validate inputs
     $num_pattern = '/\d/';
     
+    //all following if statements are validating the fields edited by the user to make sure
+    //they contain the correct data types
     if (is_string($name) && !(preg_match($num_pattern, $name)) == 1) {
       $approved++;
     }
@@ -47,23 +48,24 @@
       $approved++;
     }
     
-    
+    //if all fields have been approved, the data will be inserted into the database to create a new department
     if ($approved == 3) {
-      $sql = "INSERT INTO technonauts(DepartmentId,Deptname,Manager, Location)
-      VALUES (‘$id’, ‘$name', ‘$manager', '$location')";
       
-      if ($conn->query($sql) === TRUE) {
-        echo "New department added successfully";
-      } else {
-        echo "Operation unsuccessful: Department was not added, please try again.";
-        echo "Error: " . $sql . "<br>" . $conn->error;
-      }
+      $num_id=(int)$_POST['id'];
+      $insert=array(
+        '_id'=>$num_id,
+        'name'=>$_POST['name'],
+        'manager'=>$_POST['manager'],
+        'location'=>$_POST['location'],
+      );
+      $collection->insertOne($insert);
     }
     else {
       echo "Operation unsuccessful: Department was not added, please try again.";
     }
     ?>
 
+    <!--Displays the back bar and the copyright bar at the bottom-->
     <div class = banner>
       <a href= "./index.php"> Back </a>
     </div>
@@ -72,9 +74,7 @@
       The Techonauts: Bonnie Atelsek - Matthew Kabat - Nathan Murzello - Terrence Gaines
     </div>
     
-    <?php
-      $conn->close();
-    ?>
+    
   </body>
 
 </html>
